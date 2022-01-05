c421ddf7f5ca2a42d91c8ba826e7006a840c88bc
poemhave <96159074+poemhave@users.noreply.github.com>
2022-01-06 04:48:12 +0900

    Create KimSiYoung_EX10.cs

---
+++ KimSiYoung_EX10.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KimSiYoung : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            Debug.Log("★☆☆☆☆");
+            Debug.Log("★★☆☆☆");
+            Debug.Log("★★★☆☆");
+            Debug.Log("★★★★☆");
+            Debug.Log("★★★★★");
+        }
+    }
+    void Update()
+    {
+
+
+    }
+}
